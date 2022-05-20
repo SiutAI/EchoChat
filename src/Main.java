38e6341db5dff3ab93d655aae1b6bd152ac1c262
Satius D <syutkinaai@gmail.com>
2022-05-20 20:46:19 +0300

    init project

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // создаем серверный сокет на порту 1234
+        ServerSocket server = new ServerSocket(1234);
+        while(true) {
+            System.out.println("Waiting...");
+            // ждем клиента из сети
+            Socket socket = server.accept();
+            System.out.println("Client connected!");
+            // создаем клиента на своей стороне и запускаем поток
+            new Thread (new Client(socket)).start();
+        }
+    }
+
+}
